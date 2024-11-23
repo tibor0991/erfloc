4df6646216a1447b3afa01da23b678e99199abe9
Gianfranco Damato <gianfranco.damato90@gmail.com>
2024-11-23 01:26:48 +0100

    Serving forms and responses

--- erfloc/main.py
+++ erfloc/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
-from erfloc.routes import coords
+from erfloc.routes import coords, forms
+
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 app.include_router(coords.router, prefix='/api/coords')
+app.include_router(forms.router)
+
+app.mount("/static", StaticFiles(directory="erfloc/statics", packages=["erfloc"], html=True), name="static")
---
+++ erfloc/routes/forms.py
@@ -0,0 +1,34 @@
+from fastapi import Form, APIRouter, Request
+from datetime import datetime
+from typing import Annotated
+from erfloc.dependencies.geoloc import GeolocatorDep
+from fastapi.templating import Jinja2Templates
+from jinja2 import PackageLoader, Environment
+
+
+template = Jinja2Templates(env=Environment(
+    loader=PackageLoader('erfloc')
+))
+
+router = APIRouter()
+
+@router.post("/site/get-coords/")
+async def get_coords(request: Request, location: Annotated[str, Form()], observation_time: Annotated[datetime, Form()], geolocator: GeolocatorDep):
+    coords = geolocator.get_eci_coords(location, observation_time)
+    return template.TemplateResponse(
+        request=request,
+        name="eci_coords.html.j2",
+        context={
+            'location': coords.location,
+            'obstime': coords.obstime,
+            'coords': {
+                'x': coords.coords.x,
+                'y': coords.coords.y,
+                'z': coords.coords.z
+            }
+        }
+    )
+
+
+
+
