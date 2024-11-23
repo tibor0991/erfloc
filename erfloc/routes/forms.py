c2094c978f3d414ea9bc650dbd14dc03fe310464
Gianfranco Damato <gianfranco.damato90@gmail.com>
2024-11-23 01:38:53 +0100

    Adding dependencies to the route

--- erfloc/routes/forms.py
+++ erfloc/routes/forms.py
@@ -2,18 +2,12 @@ from fastapi import Form, APIRouter, Request
 from datetime import datetime
 from typing import Annotated
 from erfloc.dependencies.geoloc import GeolocatorDep
-from fastapi.templating import Jinja2Templates
-from jinja2 import PackageLoader, Environment
-
-
-template = Jinja2Templates(env=Environment(
-    loader=PackageLoader('erfloc')
-))
+from erfloc.dependencies.templates import TemplateDep
 
 router = APIRouter()
 
 @router.post("/site/get-coords")
-async def get_coords(request: Request, location: Annotated[str, Form()], observation_time: Annotated[datetime, Form()], geolocator: GeolocatorDep):
+async def get_coords(request: Request, location: Annotated[str, Form()], observation_time: Annotated[datetime, Form()], geolocator: GeolocatorDep, template: TemplateDep):
     coords = geolocator.get_eci_coords(location, observation_time)
     return template.TemplateResponse(
         request=request,
