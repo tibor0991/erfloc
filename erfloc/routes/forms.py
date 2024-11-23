353ad741e13be834dcf7d977013ae3a495be5082
Gianfranco Damato <gianfranco.damato90@gmail.com>
2024-11-23 01:31:47 +0100

    Changed how the variables are fed to the Jinja2 template

--- erfloc/routes/forms.py
+++ erfloc/routes/forms.py
@@ -12,21 +12,13 @@ template = Jinja2Templates(env=Environment(
 
 router = APIRouter()
 
-@router.post("/site/get-coords/")
+@router.post("/site/get-coords")
 async def get_coords(request: Request, location: Annotated[str, Form()], observation_time: Annotated[datetime, Form()], geolocator: GeolocatorDep):
     coords = geolocator.get_eci_coords(location, observation_time)
     return template.TemplateResponse(
         request=request,
         name="eci_coords.html.j2",
-        context={
-            'location': coords.location,
-            'obstime': coords.obstime,
-            'coords': {
-                'x': coords.coords.x,
-                'y': coords.coords.y,
-                'z': coords.coords.z
-            }
-        }
+        context={**coords.model_dump()}
     )
 
 
